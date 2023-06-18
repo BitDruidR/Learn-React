6c6a4e535e7e1fc6a99b9905167d5c36ddcf48d7
Roland Balt <61509877+BitDruidR@users.noreply.github.com>
2023-06-18 18:59:06 +0200

    Initialization of ToDo test application
    This app is a test project to train the react concepts I learn from online tutorials.

---
+++ to-do-list/src/Item/Item.js
@@ -0,0 +1,10 @@
+import classes from './Item.module.css'
+function Item(){
+    return(
+        <div className = {classes.item}>
+            <p>Hallo World!</p>
+        </div>
+    );
+}
+
+export default Item;
